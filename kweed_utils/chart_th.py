b8d823422ee6a8270ddb4c9d4632d9a8764a8e1d
KodexArg <gcavedal@gmail.com>
2023-01-18 01:28:59 -0300

    Fix: humidity quantile added...

--- kweed_utils/chart_th.py
+++ kweed_utils/chart_th.py
@@ -48,8 +48,8 @@ def resample_by_time(df: pd.DataFrame, hours: int = 24, tu: str = "1H") -> pd.Da
     # Lower and upper bounds and set outliers to NaN in order to discard errors from adafruit_dht
     lb_temp = rdf["temp"].quantile(0.01)
     ub_temp = rdf["temp"].quantile(0.99)
-    lb_humi = rdf["humi"].quantile(0.01)
-    ub_humi = rdf["humi"].quantile(0.99)
+    lb_humi = rdf["humi"].quantile(0.05)
+    ub_humi = rdf["humi"].quantile(0.95)
 
     logger.debug("Handling temperature outliers values")
     rdf = rdf[(rdf["temp"] > lb_temp) & (rdf["temp"] < ub_temp)]
