281c6e6a00b35b636ea0fc72be4c02643f92b31d
KodexArg <gcavedal@gmail.com>
2023-01-22 23:24:55 -0300

    load_dotenv() fixed

--- kweed_utils/getth.py
+++ kweed_utils/getth.py
@@ -9,10 +9,10 @@ ATTEMPT = 5
 TIME_RETRY = 1
 
 dotenv.load_dotenv()
-if os.getenv("DHT") == 11:
-    dht = adafruit_dht.DHT11(os.getenv("BOARD"), use_pulseio=False)
+if os.getenv("DHT") == "11":
+    dht = adafruit_dht.DHT11(os.getenv("BOARD"))
 else:
-    dht = adafruit_dht.DHT22(os.getenv("BOARD"), use_pulseio=False)
+    dht = adafruit_dht.DHT22(os.getenv("BOARD"))
 
 
 def dht_reading(ask_for):
